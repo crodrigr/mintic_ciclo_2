b18a1aca6ffc89d56790cdc6032f3599e7dfd6f7
Camilo Rodriguez <crodrigr@gmail.com>
2021-08-07 20:56:03 -0500

    07-08-2021

--- Semana_3/Ejercicios_3/Ciclo_2_3_83_Libreria/src/ciclo_2_3_83_libreria/Libreria.java
+++ Semana_3/Ejercicios_3/Ciclo_2_3_83_Libreria/src/ciclo_2_3_83_libreria/Libreria.java
@@ -10,7 +10,8 @@ public class Libreria {
     public static void main(String[] args) {
 
         ArrayList<Publicacion> publicaciones = new ArrayList<Publicacion>();
-        Ventas ventas=new Ventas();
+        loadData(publicaciones);
+        Ventas ventas = new Ventas();
 
         int op = 0;
         do {
@@ -29,9 +30,9 @@ public class Libreria {
                     mostrarPublicaciones(publicaciones);
                     break;
                 case 4:
-                      System.out.println("Guardar venta");
-                      int idPublicacion=getIdVenta(publicaciones);
-                      ventas.agregarVenta(publicaciones.get(idPublicacion));
+                    System.out.println("Guardar venta");
+                    int idPublicacion = getIdVenta(publicaciones);
+                    ventas.agregarVenta(publicaciones.get(idPublicacion));
                     break;
                 case 5:
                     System.out.println("Listar todas ventas");
@@ -43,23 +44,49 @@ public class Libreria {
 
     }
 
+    public static void loadData(ArrayList<Publicacion> publicaciones) {
+        Libro l1 = new Libro(255, 2021, "Java", 89000.2f);
+        Libro l2 = new Libro(1250, 2021, "C#", 45000.2f);
+        Libro l3 = new Libro(2890, 2021, "Javascript", 78000.2f);
+        Libro l4 = new Libro(1582, 2021, "Python", 35000.2f);
+        Disco d1 = new Disco(56.56f, "Vallenato", 35000.2f);
+        Disco d2 = new Disco(86.56f, "Rock", 75000.2f);
+        Disco d3 = new Disco(78.56f, "Ranchera", 89000.2f);
+        Disco d4 = new Disco(63.56f, "Clasica", 25000.2f);
+        publicaciones.add(l1);
+        publicaciones.add(l2);
+        publicaciones.add(l3);
+        publicaciones.add(l4);
+        publicaciones.add(d1);
+        publicaciones.add(d2);
+        publicaciones.add(d3);        
+        publicaciones.add(d4);
+       
+    }
+
     public static void mostrarPublicaciones(ArrayList<Publicacion> publicaciones) {
         for (Publicacion p : publicaciones) {
             System.out.println("-----------------------------");
+            if (p instanceof Libro) {
+                System.out.println("Este objeto es un libro");
+            }
+            if (p instanceof Disco) {
+                System.out.println("Este objeto es un disco");
+            }
             p.mostrar();
         }
 
     }
-    
+
     public static int getIdVenta(ArrayList<Publicacion> publicaciones) {
-        int numPublicacion=0;
+        int numPublicacion = 0;
         for (Publicacion p : publicaciones) {
-            System.out.println("----------------"+ numPublicacion);
+            System.out.println("----------------" + numPublicacion);
             p.mostrar();
-            numPublicacion=numPublicacion+1;
+            numPublicacion = numPublicacion + 1;
         }
         System.out.println("Ingrese el Id de la publicación para guardar venta");
-        int idPublicacion=leer.nextInt();
+        int idPublicacion = leer.nextInt();
         return idPublicacion;
     }
 
